98301ddd3e9bf4e1837faff4d9ba391f94dd9194
PrathameshPGAGI <pratamesh@pgagi.in>
2025-07-31 02:17:49 +0530

    Enable audio data storage in MongoDB by uncommenting insertion logic in websocket_audio_endpoint

--- server.py
+++ server.py
@@ -110,12 +110,11 @@ async def websocket_audio_endpoint(websocket: WebSocket):
                 ws_message = json.loads(message)
                 
                 if ws_message.get('event') == 'audio_mixed_raw.data':
-                    # audio_collection.insert_one({
-                    #     "bot_id": ws_message['bot']['id'],
-                    #     "buffer": ws_message['data']['buffer'],
-                    #     "timestamp": ws_message['data']['timestamp']
-                    # })
-                    print(ws_message)
+                    audio_collection.insert_one({
+                        "bot_id": ws_message['bot']['id'],
+                        "buffer": ws_message['data']['buffer'],
+                        "timestamp": ws_message['data']['timestamp']
+                    })
             
                 else:
                     print(f"Unhandled WebSocket event: {ws_message.get('event')}")
